fd6a5aa05c858cd2fcef0a8c1d74e0246555d8a3
Leandro Reis <lleandrofr@gmail.com>
2023-01-19 22:53:28 -0300

    feat: return only completedHabits habits_id in the get day request

--- src/routes.ts
+++ src/routes.ts
@@ -62,9 +62,11 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
 
+    const completedHabits = day?.dayHabits.map((dayHabit) => dayHabit.habit_id);
+
     return {
       possibleHabits,
-      day,
+      completedHabits,
     };
   });
 }
